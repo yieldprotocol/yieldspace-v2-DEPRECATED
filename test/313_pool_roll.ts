eaa5e98f0577bb2af1fa0cab6b7c3822c198c433
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-03-16 22:56:18 +0000

    fix: roll test uses new sell interface

--- test/313_pool_roll.ts
+++ test/313_pool_roll.ts
@@ -101,9 +101,9 @@ describe('Pool', async function () {
     const fyToken2Before = await fyToken2.balanceOf(owner)
     const baseTokenPool2Before = await base.balanceOf(pool2.address)
     await fyToken1.transfer(pool1.address, fyTokenIn)
-    await pool1.sellFYToken(pool2.address, fyTokenIn)
+    await pool1.sellFYToken(pool2.address)
     const exactBaseIn = (await base.balanceOf(pool2.address)).sub(baseTokenPool2Before)
-    await pool2.sellBaseToken(owner, exactBaseIn)
+    await pool2.sellBaseToken(owner)
 
     almostEqual((await fyToken2.balanceOf(owner)).sub(fyToken2Before), fyTokenOut, fyTokenIn.div(1000000))
   })
