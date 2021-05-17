27d7317ed1f453c83bdc07bf024212fc12d615df
uivlis <vladsilviufarcas@gmail.com>
2021-05-17 13:39:45 +0300

    Additional renaming

--- contracts/PoolBridge.sol
+++ contracts/PoolBridge.sol
@@ -36,31 +36,31 @@ contract PoolBridge {
     /// @dev Burn liquidity tokens and receive base tokens and fyTokens.
     /// The trader needs to have called `src.approve`
     /// @param src Pool
-    /// @param amount Amount of pool tokens
+    /// @param poolTokenIn Amount of pool tokens
     /// @param minBaseTokenOut The minimum amount of base tokens returned.
     /// @param minFYTokenOut The minimum amount of fyTokens returned.
     /// @return The amount of reserve tokens burned and returned (tokensBurned, baseTokens, fyTokens).
-    function burn(address src, uint256 amount, uint256 minBaseTokenOut, uint256 minFYTokenOut)
+    function burn(address src, uint256 poolTokenIn, uint256 minBaseTokenOut, uint256 minFYTokenOut)
         external
         returns (uint256, uint256, uint256)
     {
-        IERC20(src).transferFrom(msg.sender, src, amount);
+        IERC20(src).transferFrom(msg.sender, src, poolTokenIn);
         return IPool(src).burn(msg.sender, minBaseTokenOut, minFYTokenOut);
     }
 
     /// @dev Transfer all liquidity from an expired pool to the target `dst` pool
     /// The trader needs to have called `src.approve`
     /// @param src Pool
-    /// @param amount The amount of pool tokens
+    /// @param poolTokenBridged The amount of pool tokens
     /// @param dst Target pool
     /// @param fyTokenToBuy The amount of fyTokens liquified in the target pool
     /// @param minTokensMinted Minimum amount of target liquidity tokens received.
     /// @return The amount of target liquidity tokens minted.
-    function bridge(address src, address dst, uint256 amount, uint256 fyTokenToBuy, uint256 minTokensMinted)
+    function bridge(address src, address dst, uint256 poolTokenBridged, uint256 fyTokenToBuy, uint256 minTokensMinted)
         external
         returns (uint256, uint256, uint256)
     {
-        IERC20(src).transferFrom(msg.sender, src, amount);
+        IERC20(src).transferFrom(msg.sender, src, poolTokenBridged);
         (, uint256 baseTokenOut) = IPool(src).burnForBaseToken(address(this), 0);
         IERC20(IPool(src).baseToken()).transfer(dst, baseTokenOut);
         return IPool(dst).mintWithBaseToken(msg.sender, fyTokenToBuy, minTokensMinted);
